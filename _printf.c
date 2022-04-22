f34c7ad842dfd6cfc1d6581c65584ebda26b749e
BennyMbang <mbenny511@gmail.com>
2022-04-22 06:26:38 -0700

    all tasks done

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
 
 /**
  * _printf - formatted output conversion and print data.
@@ -49,51 +48,4 @@ int _printf(const char *format, ...)
 	}
 	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 	return (len);
-=======
-/**
-* _printf - main function to print in console
-* @format: array to print and check type
-* Return: count of character printed
-**/
-int _printf(const char *format, ...)
-{
-	int count = -1;
-
-	if (format != NULL)
-	{
-		int i;
-		va_list ar_list;
-		int (*o)(va_list);
-
-		va_start(ar_list, format);
-
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-
-		count = 0;
-
-		for (i = 0; format[i] != '\0'; i++)
-		{
-			if (format[i] == '%')
-			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]);
-					i++;
-				}
-				else if (format[i + 1] != '\0')
-				{
-					o = get_func(format[i + 1]);
-					count += (o ? o(ar_list) : _putchar(format[i]) + _putchar(format[i + 1]));
-					i++;
-				}
-			}
-			else
-				count += _putchar(format[i]);
-		}
-		va_end(ar_list);
-	}
-
-	return (count);
->>>>>>> d1035744e67f79960b7fc0f9fa0543e21e74caea
 }
--- print_rev.c
+++ print_rev.c
@@ -1,33 +1,18 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * print_rot - writes the str in ROT13
-=======
  * print_rev - writes the str in reverse
->>>>>>> c45889d7a409f478183e40ccec994ca4101835cf
  * @arguments: input string
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
  */
-<<<<<<< HEAD
-
-int print_rot(va_list arguments, char *buf, unsigned int ibuf)
-{
-	char alf[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	char *str;
-	unsigned int i, j, k;
-	char nill[] = "(avyy)";
-=======
 int print_rev(va_list arguments, char *buf, unsigned int ibuf)
 {
 	char *str;
 	unsigned int i;
 	int j = 0;
 	char nill[] = "(llun)";
->>>>>>> c45889d7a409f478183e40ccec994ca4101835cf
 
 	str = va_arg(arguments, char *);
 	if (str == NULL)
@@ -37,26 +22,11 @@ int print_rev(va_list arguments, char *buf, unsigned int ibuf)
 		return (6);
 	}
 	for (i = 0; str[i]; i++)
-<<<<<<< HEAD
-	{
-		for (k = j = 0; alf[j]; j++)
-		{
-			if (str[i] == alf[j])
-			{
-				k = 1;
-				ibuf = handl_buf(buf, rot[j], ibuf);
-				break;
-			}
-		}
-		if (k == 0)
-			ibuf = handl_buf(buf, str[i], ibuf);
-=======
 		;
 	j = i - 1;
 	for (; j >= 0; j--)
 	{
 		ibuf = handl_buf(buf, str[j], ibuf);
->>>>>>> c45889d7a409f478183e40ccec994ca4101835cf
 	}
 	return (i);
 }
